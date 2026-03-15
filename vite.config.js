4dc15910e7dc5c153219a19118d4233c0b26b7e6
AltF0x4 <dumitru.silviu@proton.me>
2026-03-15 18:24:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This tells Vite that your app will be hosted at AltF0x4.github.io/ACUL-Items/
-  base: '/ACUL-Items/', 
+  // MUST be your absolute GitHub Pages URL to prevent Auth0 domain hijacking
+  base: 'https://AltF0x4.github.io/ACUL-Items/', 
 })
