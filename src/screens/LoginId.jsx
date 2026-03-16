b6c4c82cc2f7b9a2fab4f9324d397468671c08f0
AltF0x4 <dumitru.silviu@proton.me>
2026-03-16 13:01:56 +0200

    Update LoginId.jsx

--- src/screens/LoginId.jsx
+++ src/screens/LoginId.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import LoginId from '@auth0/auth0-acul-js/login-id';
+import { useLoginId } from '@auth0/auth0-acul-react/login-id';
 
 export default function LoginIdPrompt({ appData }) {
-  const screenProvider = new LoginId();
+  const { login } = useLoginId();
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    const submitBtn = event.target.querySelector('button[type="submit"]');
-    if (submitBtn) submitBtn.setAttribute("disabled", "true");
-
-    const identifierInput = event.target.querySelector('input[name="username"]').value;
-    screenProvider.login({ username: identifierInput });
+    const identifierInput = event.target.username.value;
+    // Use the React Hook to submit
+    login({ username: identifierInput });
   };
 
   return (
     <div className="login-card">
       <div className="branding-header">
-        {/* Render the dynamic logo and name */}
-        {appData.logo && <img src={appData.logo} alt={`${appData.name} Logo`} className="app-logo" />}
+        {appData.logo && <img src={appData.logo} alt="Logo" className="app-logo" />}
         <h1 className="app-title">{appData.name}</h1>
         <p className="app-subtitle">Please enter your email to continue</p>
       </div>
